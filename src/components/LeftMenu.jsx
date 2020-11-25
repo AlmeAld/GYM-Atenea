d89c6636149d822ecc30f6ff4b89c96201cde597
almeald <almenaldana@gmail.com>
2020-11-25 14:28:09 -0500

    creando la base de mainLayout

---
+++ src/components/LeftMenu.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./leftMenu.scss";
+
+function LeftMenu() {
+  return <div>aqui va ir el menu de la parte izquierda</div>;
+}
+
+export default LeftMenu;
---
+++ src/container/ResumenContainer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./resumenContainer.scss";
+
+function ResumenContainer() {
+  return <div className="prueba">ResumenContainer</div>;
+}
+
+export default ResumenContainer;
