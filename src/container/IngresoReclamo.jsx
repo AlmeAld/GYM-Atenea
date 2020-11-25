da49e51dcc5643084788436626b5bc32eba724b4
Natalia Rodriguez <nataliarodriguez@MacBook-Pro-de-Natalia.local>
2020-11-25 15:26:56 -0500

    ingreso

--- src/container/IngresoReclamo.jsx
+++ src/container/IngresoReclamo.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./ingresoReclamo.scss";
+import MainLayout from "../components/layout/MainLayout"
 
 function IngresoReclamo() {
-  return <div className="prueba">loginContainer</div>;
+  return <MainLayout><div className="prueba"> Resumen container
+    </div>;
+
+
+  </MainLayout>;
+
 }
 
 export default IngresoReclamo;
