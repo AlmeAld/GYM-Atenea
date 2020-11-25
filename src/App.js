8f82798fe39de2e0d5505b17c7f8839196263911
almeald <almenaldana@gmail.com>
2020-11-25 14:47:15 -0500

    comentando codigo

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ function App() {
         <Route path="/ReportesGYM">
           <ReportesGYM />
         </Route>
-        <Route path="/GestorDocumental" component={LoginContainer}></Route>
+        {/* <Route path="/GestorDocumental" component={LoginContainer}></Route> */}
       </Switch>
     </Router>
   );
