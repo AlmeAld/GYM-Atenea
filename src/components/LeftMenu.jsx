76f391e3ba156b384e1a37654c2a4219d958a352
lalopez13 <63772079+lalopez13@users.noreply.github.com>
2020-11-26 00:17:52 -0500

    Navbar v2

--- src/components/LeftMenu.jsx
+++ src/components/LeftMenu.jsx
@@ -4,12 +4,16 @@ import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import IconButton from '@material-ui/core/IconButton';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import SpotifyLogo from '../assets/spotify.png';
+import Logo from '../assets/Logo-Regional-Blanco.png'
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import Collapse from '@material-ui/core/Collapse';
+import StarBorder from '@material-ui/icons/StarBorder';
 import MailIcon from '@material-ui/icons/Mail';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
@@ -28,17 +32,28 @@ const useStyles = makeStyles((theme) => ({
   },
   drawer: {
     width: drawerWidth,
+    backgroundColor: 'black',
     flexShrink: 0,
     whiteSpace: 'nowrap',
   },
+  ListItemText:{
+    color:'white'
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
   drawerOpen: {
     width: drawerWidth,
+    backgroundColor: 'black',
+    color: 'white',
     transition: theme.transitions.create('width', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
   },
   drawerClose: {
+    backgroundColor: 'black',
+    color: 'white',
     transition: theme.transitions.create('width', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
@@ -49,30 +64,18 @@ const useStyles = makeStyles((theme) => ({
       width: theme.spacing(9) + 1,
     },
   },
-  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    height:"150px",
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-}));
+  logoSpotify:{
+    width:40
+  }
+ }));
 
 function LeftMenu(props) {
   const classes = useStyles();
+  const [listOpen, setListOpen] = React.useState(false);
 
+  const handleClick = () => {
+    setListOpen(!listOpen);
+  };
   const theme = useTheme();
   return (
     <Drawer
@@ -88,29 +91,136 @@ function LeftMenu(props) {
         }),
       }}
     >
-      <div className={classes.toolbar}>
-        <IconButton onClick={props.handleDrawerClose}>
-          {theme.direction === 'rtl' ? (
-            <ChevronRightIcon />
-          ) : (
-            <ChevronLeftIcon />
-          )}
-        </IconButton>
-        <div className="logo-cont"></div>
+      <div 
+      // className={classes.toolbar}
+      >
+        <div onClick={props.handleDrawerClose} className="logo-cont">
+         <img src={Logo} alt="" className="logo"/>
+
+        </div>
       </div>
       
       <Divider />
       <List>
-        {['Resumen', 'Reclamos', 'Proovedores', 'Chat interno','Reportes','Gestor documental'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
+      <ListItem button>
+          <ListItemIcon id="icon">
+            <InboxIcon />
+          </ListItemIcon>
+            <ListItemText primary={"Resumen"} />
           </ListItem>
-        ))}
+
+          <ListItem button onClick={handleClick}>
+        <ListItemIcon id="icon">
+          <InboxIcon />
+        </ListItemIcon>
+        <ListItemText primary="Reclamos" />
+        {listOpen ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={listOpen} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItem button 
+          // className={classes.nested}
+          >
+            <ListItemIcon id="icon">
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="Ingresar reclamo" />
+          </ListItem>
+        </List>
+        <List component="div" disablePadding>
+          <ListItem button 
+          // className={classes.nested}
+          >
+            <ListItemIcon id="icon">
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="Seguimiento reclamo" />
+          </ListItem>
+        </List>
+      </Collapse>
+
+      <ListItem button>
+          <ListItemIcon id="icon">
+            <InboxIcon />
+          </ListItemIcon>
+            <ListItemText primary={"Proveedores"} />
+          </ListItem>
+
+          <ListItem button onClick={handleClick}>
+        <ListItemIcon id="icon">
+          <MailIcon clasName="icon-side-menu" />
+        </ListItemIcon>
+        <ListItemText primary="Chat interno" />
+        {listOpen ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={listOpen} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItem button 
+          // className={classes.nested}
+          >
+            <ListItemIcon id="icon">
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="Firma por defecto" />
+          </ListItem>
+        </List>
+        <List component="div" disablePadding>
+          <ListItem button 
+          // className={classes.nested}
+          >
+            <ListItemIcon id="icon">
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="Texto Legal" />
+          </ListItem>
+        </List>
+        <List component="div" disablePadding>
+          <ListItem button >
+            <ListItemIcon id="icon">
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="Nueva comunicación" />
+          </ListItem>
+        </List>
+      </Collapse>
+
+      <ListItem button onClick={handleClick}>
+        <ListItemIcon id="icon">
+          <InboxIcon />
+        </ListItemIcon>
+        <ListItemText primary="Reportes" />
+        {listOpen ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={listOpen} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItem button >
+            <ListItemIcon id="icon">
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="Proveedor--GYM" />
+          </ListItem>
+        </List>
+        <List component="div" disablePadding>
+          <ListItem button >
+            <ListItemIcon id="icon">
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="GYM--Proveedor" />
+          </ListItem>
+        </List>
+      </Collapse>
+
+      <ListItem button>
+          <ListItemIcon id="icon">
+            <InboxIcon />
+          </ListItemIcon>
+            <ListItemText primary={"Gestor documental"} />
+          </ListItem>     
       </List>
       <Divider />
+      <div className="divLogo">
+       <img src={SpotifyLogo} alt="" className={classes.logoSpotify} />
+      </div>
     </Drawer>
   );
 }
