b246e1fd97ae485ef707c2788aaa3f1fd9d307c8
lalopez13 <63772079+lalopez13@users.noreply.github.com>
2020-11-25 20:03:45 -0500

    preview NavBar

--- src/components/LeftMenu.jsx
+++ src/components/LeftMenu.jsx
@@ -1,8 +1,118 @@
-import React from "react";
-import "./leftMenu.scss";
+import React from 'react';
+import './leftMenu.scss';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
 
-function LeftMenu() {
-  return <div>aqui va ir el menu de la parte izquierda</div>;
+const drawerWidth = 240;
+// estilos de ejemplo ya estoy revisando la documentacion para hacerlos con sass
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+   menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    height:"150px",
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+}));
+
+function LeftMenu(props) {
+  const classes = useStyles();
+
+  const theme = useTheme();
+  return (
+    <Drawer
+      variant="permanent"
+      className={clsx(classes.drawer, {
+        [classes.drawerOpen]: props.open,
+        [classes.drawerClose]: !props.open,
+      })}
+      classes={{
+        paper: clsx({
+          [classes.drawerOpen]: props.open,
+          [classes.drawerClose]: !props.open,
+        }),
+      }}
+    >
+      <div className={classes.toolbar}>
+        <IconButton onClick={props.handleDrawerClose}>
+          {theme.direction === 'rtl' ? (
+            <ChevronRightIcon />
+          ) : (
+            <ChevronLeftIcon />
+          )}
+        </IconButton>
+        <div className="logo-cont"></div>
+      </div>
+      
+      <Divider />
+      <List>
+        {['Resumen', 'Reclamos', 'Proovedores', 'Chat interno','Reportes','Gestor documental'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </Drawer>
+  );
 }
 
 export default LeftMenu;
