a18e52a04fc6c4ad6caeca0a1773f9051416ad11
almeald <almenaldana@gmail.com>
2020-11-27 15:35:31 -0500

    fixed eslint issues

--- src/components/MyDocument.jsx
+++ src/components/MyDocument.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Document, Page, Text, View, StyleSheet } from "@react-pdf/renderer";
+import { Document, Page, Text, StyleSheet } from "@react-pdf/renderer";
 
 const MyDocument = () => (
   <Document>
     <Page style={styles.body}>
-
       <Text style={styles.title}>Documento Proveedor</Text>
       <br></br>
       <br></br>
