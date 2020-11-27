8631c043a206a1f0c4253c0c59b6afc11078ad8e
almeald <almenaldana@gmail.com>
2020-11-27 02:04:36 -0500

    enrutado dinamico para seguimientosReclamos:id

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Redirect,
   Route,
   Switch,
 } from "react-router-dom";
+import fire from "./firebase";
 import LoginContainer from "./container/LoginContainer";
 import Comunicaciones from "./container/Comunicaciones";
 import IngresoReclamo from "./container/IngresoReclamo";
@@ -12,10 +13,13 @@ import Proveedores from "./container/Proveedores";
 import Reportesproveedores from "./container/ReporteProveedores";
 import Reclamos from "./container/Reclamos";
 import ResumenContainer from "./container/ResumenContainer";
+import SeguimientoReclamoUser from "./container/SeguimientoReclamoUser";
 import SeguimientoReclamoContainer from "./container/SeguimientoReclamoContainer";
+import Loader from "./components/Loader";
 
 import "./App.css";
 import MainLayout from "./components/layout/MainLayout";
+// import LoginLayout from "./components/layout/MainLayout";
 
 //esta funcion las vamos a usar cuando sepamos el nombre de las rutas
 function LoggedInRoute({ children, isUserLoggedIn, ...rest }) {
@@ -41,7 +45,7 @@ function NonLoggedInRoute({ children, isUserLoggedIn, ...rest }) {
         if (!isUserLoggedIn) {
           return <>{children}</>;
         }
-        //return <Redirect to='/ruta' />
+        return <Redirect to="/Resumen" />;
       }}
     />
   );
@@ -50,37 +54,70 @@ function NonLoggedInRoute({ children, isUserLoggedIn, ...rest }) {
 function App() {
   // este estado es para saber si el usuario inició sesión o no y de acuerdo a eso lo va a redireccionar segun la ruta que corresponda
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(true);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setIsUserLoggedIn(true);
+        console.log("logeado");
+      } else {
+        setIsUserLoggedIn(false);
+        console.log("no esta logeado");
+      }
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) return <Loader />;
 
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
+        <NonLoggedInRoute isUserLoggedIn={isUserLoggedIn} exact path="/">
           <LoginContainer />
-        </Route>
-        <Route path="/Resumen">
+        </NonLoggedInRoute>
+        <LoggedInRoute isUserLoggedIn={isUserLoggedIn} path="/Resumen">
           <ResumenContainer />
-        </Route>
-        <Route path="/IngresoReclamos">
+        </LoggedInRoute>
+        <LoggedInRoute isUserLoggedIn={isUserLoggedIn} path="/IngresoReclamos">
           <IngresoReclamo />
-        </Route>
-        <Route path="/SeguimientosReclamos">
+        </LoggedInRoute>
+        <LoggedInRoute
+          isUserLoggedIn={isUserLoggedIn}
+          exact
+          path="/SeguimientosReclamos:id"
+        >
+          <SeguimientoReclamoUser />
+        </LoggedInRoute>
+        <LoggedInRoute
+          isUserLoggedIn={isUserLoggedIn}
+          exact
+          path="/SeguimientosReclamos"
+        >
           <SeguimientoReclamoContainer />
-        </Route>
-        <Route path="/Proveedores">
+        </LoggedInRoute>
+        <LoggedInRoute isUserLoggedIn={isUserLoggedIn} path="/Proveedores">
           <Proveedores />
-        </Route>
-        <Route path="/Comunicaciones">
+        </LoggedInRoute>
+        <LoggedInRoute isUserLoggedIn={isUserLoggedIn} path="/Comunicaciones">
           <Comunicaciones />
-        </Route>
-        <Route path="/ComunicacionesRevisar">
+        </LoggedInRoute>
+        <LoggedInRoute
+          isUserLoggedIn={isUserLoggedIn}
+          path="/ComunicacionesRevisar"
+        >
           <Comunicaciones />
-        </Route>
-        <Route path="/ReportesProveedores">
+        </LoggedInRoute>
+        <LoggedInRoute
+          isUserLoggedIn={isUserLoggedIn}
+          path="/ReportesProveedores"
+        >
           <Reportesproveedores />
-        </Route>
-        <Route path="/Reclamos">
+        </LoggedInRoute>
+        <LoggedInRoute isUserLoggedIn={isUserLoggedIn} path="/Reclamos">
           <Reclamos />
-        </Route>
+        </LoggedInRoute>
         {/* <Route path="/GestorDocumental" component={LoginContainer}></Route> */}
       </Switch>
     </Router>
