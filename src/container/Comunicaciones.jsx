02e18ea7d49e12127cf565fad463d6d9dea01d15
Natalia Rodriguez <nataliarodriguez@MacBook-Pro-de-Natalia.local>
2020-11-27 13:08:11 -0500

    Vista comunicaciones Boton abrir DIALOG, Boton imprimir PDF

--- src/container/Comunicaciones.jsx
+++ src/container/Comunicaciones.jsx
@@ -3,6 +3,8 @@ import "./comunicaciones.scss";
 import MainLayout from "../components/layout/MainLayout";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import CustomizedDialogs from "../components/OpenDialog";
+import TableComunicacion from "../components/TablaComunicacion";
 
 const gridStyles = makeStyles((theme) => ({
   root: {
@@ -19,13 +21,11 @@ function Comunicaciones() {
         <Grid container>
           <Grid item md={1}></Grid>
           <Grid item md={10}>
-            <input className="boxes">
-            </input>
-            <input className="graphicsZone">
-
-</input>
+            <TableComunicacion/>
           </Grid>
-          <Grid item md={1}></Grid>
+          <Grid item md={1}>
+          <a href=""> Ir Teams</a> 
+          </Grid>
         </Grid>
       </div>
     </MainLayout>
